1def4c000774c034d6e51c219304d111bccf7f82
fido1hn <confidoekeanya@gmail.com>
2025-04-01 20:50:06 +0100

    jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,19 @@ function App() {
   const count = 1;
   return (
     <div className="center-box">
-      <h1>Hello Challenge</h1>
-      <div>
-        <button>-</button>
-        Step: {step}
-        <button>+</button>
-      </div>
+      <div className="vertical-box">
+        <div>
+          <button>-</button>
+          Step: {step}
+          <button>+</button>
+        </div>
+        <div>
+          <button>-</button>
+          Count: {count}
+          <button>+</button>
+        </div>
 
-      <div>
-        <button>-</button>
-        Count: {count}
-        <button>+</button>
+        <p>30 days from today: </p>
       </div>
     </div>
   );
