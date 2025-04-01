ac6a8832e3c772599222cfe6d1eefc2bab65ee19
fido1hn <confidoekeanya@gmail.com>
2025-04-01 21:49:51 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import { useState } from "react";
 import "./index.css";
 
 function App() {
-  let [step, setStep] = useState(1);
+  const [step, setStep] = useState(1);
   const [count, setCount] = useState(0);
+  const date = new Date();
+  date.setDate(date.getDate() + count);
 
   function handleIncreaseStep() {
     setStep((s) => (s += 1));
@@ -24,16 +26,27 @@ function App() {
       <div className="vertical-box">
         <div>
           <button onClick={handleReduceStep}>-</button>
-          Step: {step}
+          <strong style={{ fontSize: 25, marginLeft: 10, marginRight: 10 }}>
+            Step: {step}
+          </strong>
           <button onClick={handleIncreaseStep}>+</button>
         </div>
         <div>
           <button onClick={handleReduceCount}>-</button>
-          Count: {count}
+          <strong style={{ fontSize: 25, marginLeft: 10, marginRight: 10 }}>
+            Count: {count}
+          </strong>
           <button onClick={handleIncreaseCount}>+</button>
         </div>
 
-        <p>{count} days from today: </p>
+        <p>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} day(s) from today is `
+            : `${Math.abs(count)} days ago was `}
+          {date.toDateString()}
+        </p>
       </div>
     </div>
   );
