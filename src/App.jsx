b25c9902703bd8656433f172d24d9486cc81e2c1
fido1hn <confidoekeanya@gmail.com>
2025-04-01 20:45:34 +0100

    step and count

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import "./index.css";
 
 function App() {
+  const step = 1;
+  const count = 1;
   return (
     <div className="center-box">
       <h1>Hello Challenge</h1>
+      <div>
+        <button>-</button>
+        Step: {step}
+        <button>+</button>
+      </div>
+
+      <div>
+        <button>-</button>
+        Count: {count}
+        <button>+</button>
+      </div>
     </div>
   );
 }
